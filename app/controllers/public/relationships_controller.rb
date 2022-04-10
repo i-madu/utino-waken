21c7c1be1d06f5e86bb8ec218ac88ebd9007f57f
i-madu <jinjinyouye@gmail.com>
2022-04-10 01:00:46 +0000

    [add]フォロー機能コントローラー作成

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,6 +1,25 @@
 class Public::RelationshipsController < ApplicationController
   
+  #フォローした時
+  def create
+    current_customer.follow(params[:customer_id])
+    redirect_to request.referer
+  end
   
+  #フォローを外す時
+  def destroy
+    current_customer.unfollow(params[:customer_id]).destroy
+    redirect_to request.referer
+  end
   
+  def followings
+    customer = Customer.find(params[:customer_id])
+    @customers = customer.followings
+  end
+  
+  def followers
+    customer = Customer.find(params[:customer_id])
+    @customers = customer.followers
+  end
   
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,11 +11,10 @@ class Customer < ApplicationRecord
   has_one_attached :profile_image
   #フォローした
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  #フォローされた
-  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
   has_many :followings, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_relationships, source: :follower
+  #フォローされた
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   #フォローした時
   def follow(customer_id)
